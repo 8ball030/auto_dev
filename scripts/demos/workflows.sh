d925d81cbeaba01248a625f01f2aa74a3eb38aa4
8ball030 <8baller@station.codes>
2025-01-31 11:04:02 +0000

    auto_dev:lint-and-format-code

--- scripts/demos/workflows.sh
+++ scripts/demos/workflows.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -eu pipefail
 
-poetry run adev wf  auto_dev/data/workflows/fmt_lint_workflow.yaml
+source scripts/demos/base.sh
+
+gum format "Running workflows!"
+call_and_wait "poetry run adev wf auto_dev/data/workflows/fmt_lint_workflow.yaml" 3
