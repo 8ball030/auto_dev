b188f737aa484905d36a40a3a3b0a640378a9a1c
8ball030 <8baller@station.codes>
2025-02-20 09:35:11 +0000

    temporary skip generate lock

--- scripts/check_generated.sh
+++ scripts/check_generated.sh
@@ -23,10 +23,10 @@ echo 'done autonomy'
 
 echo 'doing python'
 REPO_NAME="test_repo"
-poetry run adev repo scaffold $REPO_NAME -t python
-cd $REPO_NAME
-poetry lock --no-cache && poetry install
-cp poetry.lock ../auto_dev/data/repo/templates/python/
+# poetry run adev repo scaffold $REPO_NAME -t python
+# cd $REPO_NAME
+# poetry lock --no-cache && poetry install
+# cp poetry.lock ../auto_dev/data/repo/templates/python/
 cd ../
 rm -rf $REPO_NAME
 echo 'done python'
