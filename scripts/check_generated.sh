fcd2c492df557f8a6bc229811b1767465b33f34b
8ball030 <8baller@station.codes>
2025-02-20 09:42:31 +0000

    ensured-lock-scripts-present

--- scripts/check_generated.sh
+++ scripts/check_generated.sh
@@ -12,21 +12,21 @@ cd $REPO_NAME
 echo removing old env 
 poetry env remove --all
 echo locking
-# poetry lock --no-cache -vv || exit 0
-# echo installing
-# poetry install
-# poetry check --lock
-# cp poetry.lock ../auto_dev/data/repo/templates/autonomy/
+poetry lock --no-cache -vv || exit 0
+echo installing
+poetry install
+poetry check --lock
+cp poetry.lock ../auto_dev/data/repo/templates/autonomy/
 cd ../
 rm -rf $REPO_NAME
 echo 'done autonomy'
 
 echo 'doing python'
 REPO_NAME="test_repo"
-# poetry run adev repo scaffold $REPO_NAME -t python
-# cd $REPO_NAME
-# poetry lock --no-cache && poetry install
-# cp poetry.lock ../auto_dev/data/repo/templates/python/
+poetry run adev repo scaffold $REPO_NAME -t python
+cd $REPO_NAME
+poetry lock --no-cache && poetry install
+cp poetry.lock ../auto_dev/data/repo/templates/python/
 cd ../
 rm -rf $REPO_NAME
 echo 'done python'
