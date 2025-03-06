9af68b3f12f0ace63e3d8ebb65180566fe750b5d
8ball030 <8baller@station.codes>
2025-03-06 12:08:49 +0000

    feat:improved-deps-bumping-flow

--- auto_dev/commands/deps.py
+++ auto_dev/commands/deps.py
@@ -109,7 +109,6 @@ def from_key_to_path(key: str) -> Path:
     parts = key.split("/")
 
     path_list = [
-        "packages",
         parts[1],
         parts[0] + "s",
         parts[2],
@@ -749,6 +748,13 @@ def bump(
     else:
         click.echo("No packages.json file found. Skipping autonomy packages verification.")
         sys.exit(1)
+
+    
+    local_packages = get_package_json(packages_dir)["third_party"]
+
+    for key, package_hash in local_packages.items():
+        print(f"Removing old package: {key}")
+        remove_old_package(repo=packages_dir, proposed_dependency_updates={key: package_hash})
 
     click.echo("Verifying poetry dependencies... 📝")
     cmd, poetry_issues = get_update_command(
