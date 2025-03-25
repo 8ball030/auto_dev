510581d751c8f6d7deec9366ee9ae6832c6ebcf6
zarathustra <zarathustra_git@pm.me>
2025-03-25 14:27:23 +0100

    tests: add basic_enum.proto

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -26,6 +26,7 @@ PROTO_FILES = _get_proto_files()
     PROTO_FILES["primitives.proto"],
     PROTO_FILES["optional_primitives.proto"],
     PROTO_FILES["repeated_primitives.proto"],
+    PROTO_FILES["basic_enum.proto"],
     ])
 def test_protodantic(proto_path: Path):
 
