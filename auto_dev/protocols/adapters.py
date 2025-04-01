4487598307cd67f91db395faa669fcb06b840934
zarathustra <zarathustra_git@pm.me>
2025-04-01 22:31:36 +0200

    feat: FileAdapter.file_elements, MessageAdapter.elements and MessageAdapter.file

--- auto_dev/protocols/adapters.py
+++ auto_dev/protocols/adapters.py
@@ -14,7 +14,7 @@ from proto_schema_parser.ast import (
     Service,
     MessageElement,
     Comment,
-    Field as ProtoField,
+    Field,
     Group,
     OneOf,
     ExtensionRange,
@@ -34,16 +34,19 @@ def camel_to_snake(name: str) -> str:
 
 @dataclass
 class MessageAdapter:
+    file: FileAdapter | None = field(repr=False)
     wrapped: Message = field(repr=False)
     fully_qualified_name: str
+    elements: list[MessageElement | MessageAdapter] = field(default_factory=list, repr=False)
+
     comments: list[Comment] = field(default_factory=list)
-    fields: list[ProtoField] = field(default_factory=list)
+    fields: list[Field] = field(default_factory=list)
     groups: list[Group] = field(default_factory=list)
     oneofs: list[OneOf] = field(default_factory=list)
     options: list[Option] = field(default_factory=list)
     extension_ranges: list[ExtensionRange] = field(default_factory=list)
     reserved: list[Reserved] = field(default_factory=list)
-    messages: list[Message] = field(default_factory=list)
+    messages: list[MessageAdapter] = field(default_factory=list)
     enums: list[Enum] = field(default_factory=list)
     extensions: list[Extension] = field(default_factory=list)
     map_fields: list[MapField] = field(default_factory=list)
@@ -52,11 +55,15 @@ class MessageAdapter:
         return getattr(self.wrapped, name)
 
     @property
-    def nested_names(self) -> set[str]:
-        return {m.name for m in self.messages} | {e.name for e in self.enums}
+    def enum_names(self) -> set[str]:
+        return {m.name for m in self.enums}
+
+    @property
+    def message_names(self) -> set[str]:
+        return {m.name for m in self.messages}
 
     def qualified_type(self, type_name: str) -> str:
-        if type_name in self.nested_names:
+        if type_name in self.enum_names or type_name in self.message_names:
             return f"{self.fully_qualified_name}.{type_name}"
         return type_name
 
@@ -64,37 +71,44 @@ class MessageAdapter:
     def from_message(cls, message: Message, parent_prefix="") -> MessageAdapter:
         """Convert a `Message` into `MessageAdapter`, handling recursion."""
 
-        elements = {camel_to_snake(t.__name__): [] for t in MessageElement.__args__}
-
-        for element in message.elements:
+        elements = []
+        grouped_elements = {camel_to_snake(t.__name__): [] for t in MessageElement.__args__}
+        for i, element in enumerate(message.elements):
             key = camel_to_snake(element.__class__.__name__)
-            elements[key].append(element)
+            if isinstance(element, Message):
+                element = cls.from_message(element, parent_prefix=f"{parent_prefix}{message.name}.")
+            elements.append(element)
+            grouped_elements[key].append(element)
 
         return cls(
+            file=None,
             wrapped=message,
             fully_qualified_name=f"{parent_prefix}{message.name}",
-            comments=elements["comment"],
-            fields=elements["field"],
-            groups=elements["group"],
-            oneofs=elements["one_of"],
-            options=elements["option"],
-            extension_ranges=elements["extension_range"],
-            reserved=elements["reserved"],
-            messages=[cls.from_message(m, parent_prefix=f"{parent_prefix}{message.name}.") for m in elements["message"]],
-            enums=elements["enum"],
-            extensions=elements["extension"],
-            map_fields=elements["map_field"]
+            elements=elements,
+            comments=grouped_elements["comment"],
+            fields=grouped_elements["field"],
+            groups=grouped_elements["group"],
+            oneofs=grouped_elements["one_of"],
+            options=grouped_elements["option"],
+            extension_ranges=grouped_elements["extension_range"],
+            reserved=grouped_elements["reserved"],
+            messages=grouped_elements["message"],
+            enums=grouped_elements["enum"],
+            extensions=grouped_elements["extension"],
+            map_fields=grouped_elements["map_field"]
         )
 
 
 @dataclass
 class FileAdapter:
     wrapped: File = field(repr=False)
+    file_elements: list[FileElement | MessageAdapter] = field(repr=False)
+
     syntax: str | None
     imports: list[Import] = field(default_factory=list)
     packages: list[Package] = field(default_factory=list)
     options: list[Option] = field(default_factory=list)
-    messages: list[Message] = field(default_factory=list)
+    messages: list[MessageAdapter] = field(default_factory=list)
     enums: list[Enum] = field(default_factory=list)
     extensions: list[Extension] = field(default_factory=list)
     services: list[Service] = field(default_factory=list)
@@ -103,25 +117,47 @@ class FileAdapter:
     def __getattr__(self, name: str):
         return getattr(self.wrapped, name)
 
+    @property
+    def enum_names(self) -> set[str]:
+        return {m.name for m in self.enums}
+
+    @property
+    def message_names(self) -> set[str]:
+        return {m.name for m in self.messages}
+
     @classmethod
     def from_file(cls, file: File) -> FileAdapter:
         """Convert a `File` into `FileAdapter`, handling messages recursively."""
 
-        elements = {camel_to_snake(t.__name__): [] for t in FileElement.__args__}
-
-        for element in file.file_elements:
+        file_elements = []
+        grouped_elements = {camel_to_snake(t.__name__): [] for t in FileElement.__args__}
+        for i, element in enumerate(file.file_elements):
             key = camel_to_snake(element.__class__.__name__)
-            elements[key].append(element)
+            if isinstance(element, Message):
+                element = MessageAdapter.from_message(element)
+            file_elements.append(element)
+            grouped_elements[key].append(element)
 
-        return cls(
+        file_adapter = cls(
             wrapped=file,
+            file_elements=file_elements,
             syntax=file.syntax,
-            imports=elements["import"],
-            packages=elements["package"],
-            options=elements["option"],
-            messages=[MessageAdapter.from_message(m) for m in elements["message"]],
-            enums=elements["enum"],
-            extensions=elements["extension"],
-            services=elements["service"],
-            comments=elements["comment"]
+            imports=grouped_elements["import"],
+            packages=grouped_elements["package"],
+            options=grouped_elements["option"],
+            messages=grouped_elements["message"],
+            enums=grouped_elements["enum"],
+            extensions=grouped_elements["extension"],
+            services=grouped_elements["service"],
+            comments=grouped_elements["comment"]
         )
+
+        def set_file_adapter(message: MessageAdapter):
+            message.file = file_adapter
+            for nested_message in message.messages:
+                set_file_adapter(nested_message)
+
+        for message in file_adapter.messages:
+            set_file_adapter(message)
+
+        return file_adapter
